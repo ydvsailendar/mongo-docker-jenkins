8865e62f8398ebdddf37df1f62166ede5eab59de
yakuma <ydvsailendar0905@gmail.com>
2021-07-25 09:23:18 +0545

    pipeline file

---
+++ pipeline.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/env groovy
+pipeline {
+    agent any
+    environment {
+      DOCKER_TAG = getVersion()
+    }
+    stages{
+        stage("SCM"){
+            steps{
+                git 'https://github.com/ydvsailendar/mongo-docker-jenkins'
+            }
+        }
+        stage("Docker Build"){
+            steps{
+                sh "docker build . -t task:${DOCKER_TAG}"
+            }
+        }
+        stage("Docker Run"){
+            steps{
+                sh "docker run -d --net=host -p 5000:5000 task:${DOCKER_TAG}"
+            }
+        }
+        stage("Docker Image Cleanup"){
+            steps{
+                sh "docker image prune -a --force --filter 'until=2h'"
+            }
+        }
+    }
+}
+
+def getVersion(){
+    def commitHash = sh returnStdout: true, script: 'git rev-parse --short HEAD'
+    return commitHash
+}
+
